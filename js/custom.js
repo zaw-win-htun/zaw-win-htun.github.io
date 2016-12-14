57b446cf44d4c3cfb5a1afcc54a798990e5fe6d0
zaw-win-htun <zawwinhtun@ucsy.edu.mm>
2016-12-15 03:06:27 +0630

    email update

--- js/custom.js
+++ js/custom.js
@@ -140,57 +140,6 @@
 			return pattern.test(emailAddress);
 		};
 
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
-
-		$("#contact-form").submit(function(e) {
-
-			e.preventDefault();
-
-			var c_name = $("#c_name").val();
-			var c_email = $("#c_email").val();
-			var c_message = $("#c_message ").val();
-			var responseMessage = $('.ajax-response');
-
-			if (( c_name== "" || c_email == "" || c_message == "") || (!isValidEmailAddress(c_email) )) {
-				responseMessage.fadeIn(500);
-				responseMessage.html('<i class="fa fa-warning"></i> Check all fields.');
-			}
-
-			else {
-				$.ajax({
-					type: "POST",
-					url: "assets/php/contactForm.php",
-					dataType: 'json',
-					data: {
-						c_email: c_email,
-						c_name: c_name,
-						c_message: c_message
-					},
-					beforeSend: function(result) {
-						$('#contact-form button').empty();
-						$('#contact-form button').append('<i class="fa fa-cog fa-spin"></i> Wait...');
-					},
-					success: function(result) {
-						if(result.sendstatus == 1) {
-							responseMessage.html(result.message);
-							responseMessage.fadeIn(500);
-							$('#contact-form').fadeOut(500);
-						} else {
-							$('#contact-form button').empty();
-							$('#contact-form button').append('<i class="fa fa-retweet"></i> Try again.');
-							responseMessage.html(result.message);
-							responseMessage.fadeIn(1000);
-						}
-					}
-				});
-			}
-
-			return false;
-
-		});
-
 	});
 
 })(jQuery);
